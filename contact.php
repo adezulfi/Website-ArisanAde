99cccab696631a422035ff2c89d35c94cb4beb35
adezulfi <117923080+adezulfi@users.noreply.github.com>
2024-01-16 23:19:39 +0700

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,41 @@
+<!-- contact -->
+<div class="middle-w3l">
+	<div align="left" class="left-build-wthree py-5 px-sm-5 px-4">
+		<section class="wthree-row">
+			<div class="container">
+				<div class="text-center mb-lg-5 mb-4">
+					<h3 class="tittle mb-2">Contact Us</h3>
+					<p>PESAN SINGKAT YAAA GUYS</p>
+				</div>
+				<div class="row contact-form py-3">
+					<!-- contact map -->
+					
+					</div>
+					<!-- //contact map -->
+					<!-- contact form -->
+					<div class="col-lg-6 wthree-form-left mt-lg-0 mt-5">
+						<div class="contact-top1">
+							<form action="#" method="get" class="f-color">
+								<div class="form-group">
+									<label>Name</label>
+									<input type="text" class="contact-formw3ls form-control" name="text" id="contactusername" required>
+								</div>
+								<div class="form-group">
+									<label>Email</label>
+									<input type="email" class="contact-formw3ls form-control" name="email" id="contactemail" required>
+								</div>
+								<div class="form-group">
+									<label>Pesanmu</label>
+									<textarea class="contact-formw3ls form-control" rows="5" id="contactcomment" required></textarea>
+								</div>
+								<button type="submit" class="btn submit contact-submit">Submit</button>
+							</form>
+						</div>
+					</div>
+					<!-- //contact form -->
+				</div>
+			</div>
+		</section>
+	</div>
+</div>
+<!-- //contact -->
---
+++ content.php
@@ -0,0 +1,56 @@
+	<!-- middle section -->
+	<div class="middle-w3l">
+		<div class="container-fluid">
+			<div class="row">
+
+				<div align="left" class="left-build-wthree py-5 px-sm-5 px-4">
+					<div class="py-xl-5 py-lg-3 px-xl-4">
+						<h4 align="center">Tips Arisan Keluarga Seru</h4>
+						<h6 align="center" class="mt-3 mb-xl-5 mb-4">Ini dia 5 tips yang bisa Anda lakukan supaya arisan keluarga seru, memorable, dan tidak membosankan</h6>
+						<p>Arisan adalah momen penting yang bisa mempererat hubungan silaturahmi. Di Indonesia ada banyak jenis arisan, mulai dari arisan keluarga, arisan kantor, bahkan arisan satu kompleks perumahan. Tempat untuk arisan keluarga biasanya dilakukan di rumah salah satu anggota keluarga. Namun, ada pengalaman lain bila Anda mencari referensi tempat arisan keluarga lainnya.
+							Arisan keluarga biasanya jadi cenderung membosankan karena dilakukan rutin dengan konsep arisan yang begitu-begitu saja. Anda atau anak-anak Anda bisa jadi malas untuk datang karena hanya berkumpul, mengocok giliran, mengobrol, lalu pulang. Nyatanya, bila Anda dan keluarga mau mencoba, Anda bisa kok membuat acara yang seru dan memorable di tempat arisan keluarga.</p>
+						<ul style="padding-left:500px" class="list-beach mt-lg-5 mt-4">
+							<li>
+								
+								</div>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- //middle section -->
+
+	<!-- testimonials -->
+	<div class="testimonials py-5" id="clients">
+		<div class="container py-xl-5 py-lg-3">
+			<div class="text-center mb-lg-5 mb-4">
+				<h3 class="tittle mb-2 text-white">Peserta</h3>
+				<p class="test-title-paara">Persyaratan Peserta</p>
+			</div>
+			<div class="w3_testimonials_grids">
+				<section class="slider">
+					<div class="flexslider">
+						<ul class="slides">
+							<li>
+								<div class="w3_testimonials_grid">
+									<p>"Amanah
+										Konsisten (Tidak mengundurkan diri, setelah arisan dikocok) mengundurkan diri? Silakan cari penggantinya sendiri
+			
+
+									
+									</ul>
+
+
+								</div>
+					</div>
+					</li>
+					</ul>
+			</div>
+			</section>
+
+		</div>
+	</div>
+	</div>
+	<!-- //testimonials -->
